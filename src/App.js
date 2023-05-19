d8b0c565c5e8cded011b0545198293e5f71c134a
yoga <11211086@student.itk.ac.id>
2023-05-19 08:52:00 +0800

    perbaikan bug garis di dalam bola

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
   const [acceleration, setAcceleration] = useState(0);
   const [velocity, setVelocity] = useState(0);
   const [horizontalPos, setHorizontalPos] = useState(100);
-  const [verticalPos, setVerticalPos] = useState(100);
+  const [verticalPos, setVerticalPos] = useState(50);
 
   const [hasVelocityVector, setHasVelocityVector] = useState(false);
   const [hasAccelerationVector, setHasAccelerationVector] = useState(false);
@@ -22,49 +22,46 @@ function App() {
   return (
     <div className="min-h-[100vh] bg-sky-100">
       <div className='flex justify-center items-start'>
-
         <div className="m-10 drop-shadow-lg">
-    
             <div className='p-6 bg-slate-100'>
               <Graphic width={CANVAS_WIDTH} height={CANVAS_HEIGHT} xPos={horizontalPos} yPos={verticalPos} />
             </div>
 
             <div className='p-6 bg-slate-100 flex justify-center'>
-            <div className='flex'>
-              <div class="flex justify-center items-center rotate-180">   
-                  <button class="w-16 h-16 rounded-full bg-lime-600 focus:outline-none flex justify-center items-center ml-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"> <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z" fill='#fff'/> </svg>
-                  </button>
-                </div>
-
-
+              <div className='flex'>
                 <div class="flex justify-center items-center rotate-180">   
-                  <button class="w-20 h-20 rounded-full bg-blue-500 focus:outline-none flex justify-center items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="65" height="65" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"> <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z" fill='#fff'/> </svg>
-                  </button>
-                </div>
-          
-                <div class="flex justify-center items-center mx-3">   
-                  <input type="number" className="ml-3 bg-sky-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center text-[2rem]" value={100} />
-                </div>
+                    <button class="w-16 h-16 rounded-full bg-lime-600 focus:outline-none flex justify-center items-center ml-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"> <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z" fill='#fff'/> </svg>
+                    </button>
+                  </div>
 
-                <div class="flex justify-center items-center">   
-                  <button class="w-20 h-20 rounded-full bg-blue-500 focus:outline-none flex justify-center items-center">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="65" height="65" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"> <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z" fill='#fff'/> </svg>
-                  </button>
-                </div>
 
-                <div class="flex justify-center items-center">   
-                  <button class="w-16 h-16 rounded-full bg-lime-600 focus:outline-none flex justify-center items-center ml-2">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"> <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z" fill='#fff'/> </svg>
-                  </button>
-                </div>
+                  <div class="flex justify-center items-center rotate-180">   
+                    <button class="w-20 h-20 rounded-full bg-blue-500 focus:outline-none flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="65" height="65" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"> <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z" fill='#fff'/> </svg>
+                    </button>
+                  </div>
+            
+                  <div class="flex justify-center items-center mx-3">   
+                    <input type="number" className="ml-3 bg-sky-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-32 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 text-center text-[2rem]" value={100} />
+                  </div>
+
+                  <div class="flex justify-center items-center">   
+                    <button class="w-20 h-20 rounded-full bg-blue-500 focus:outline-none flex justify-center items-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="65" height="65" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"> <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z" fill='#fff'/> </svg>
+                    </button>
+                  </div>
+
+                  <div class="flex justify-center items-center">   
+                    <button class="w-16 h-16 rounded-full bg-lime-600 focus:outline-none flex justify-center items-center ml-2">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="currentColor" class="bi bi-play" viewBox="0 0 16 16"> <path d="M10.804 8 5 4.633v6.734L10.804 8zm.792-.696a.802.802 0 0 1 0 1.392l-6.363 3.692C4.713 12.69 4 12.345 4 11.692V4.308c0-.653.713-.998 1.233-.696l6.363 3.692z" fill='#fff'/> </svg>
+                    </button>
+                  </div>
               </div>
             </div>
         </div>
 
         <div className='bg-white rounded-lg drop-shadow-lg m-10 p-12' >
-
           <div>
             <Slider 
               title="Percepatan (m/s^2)"
@@ -83,15 +80,17 @@ function App() {
 
 
           <div className='flex justify-between mt-8'>
-          <Slider 
+              <Slider 
                 title="Posisi Horizontal"
-                maxValue={900} 
+                maxValue={950} 
+                minValue={50}
                 defaultValue={horizontalPos} 
                 onChange={(e) => setHorizontalPos(e.target.value)}
               />
               <Slider 
                 title="Posisi Vertikal"
-                maxValue={500} 
+                maxValue={660}
+                minValue={50}
                 defaultValue={verticalPos} 
                 onChange={(e) => setVerticalPos(e.target.value)}
               />
--- src/components/Graphic.js
+++ src/components/Graphic.js
@@ -7,46 +7,51 @@ export default function Graphics(props) {
 
 
     const draw = p5 => {
-        p5.clear();
+            // p5.clear();
 
         p5.background(255,255,255);
         
 
-        p5.stroke(32, 111, 153);
-        p5.fill(255,255,255)
-        p5.circle(xPos, height - yPos, 100, 100)
-
-
-        p5.stroke(217, 85, 24)
-        p5.line(xPos, height - yPos, xPos - 60, height - 50)
-
-
-        p5.stroke(24, 217, 43)
-        p5.line(xPos, height - yPos, 101, height - 120)
+        p5.stroke(255, 204, 204);
+        p5.fill(255, 204, 204)
+        p5.circle(xPos, height - yPos, 100 )
 
 
 
-        p5.stroke(217, 24, 211)
-        p5.line(xPos, height - yPos, xPos + 50, height - yPos)
 
+
+        p5.stroke(51, 51, 255)
+        p5.line(xPos, height - yPos, (xPos - width) + 1000, (height - yPos) + 50)
+
+        p5.stroke(51, 51, 255)
+        p5.line(xPos, height - yPos, xPos  , (height - yPos)  - 50)
+
+        p5.stroke(0, 255, 0)
+        p5.line(xPos, height - yPos, xPos - 50 , (height - yPos) )
+
+        p5.stroke(0, 255, 0)
+        p5.line(xPos, height - yPos, (xPos - height) + 650, (height - yPos))
+
+ 
+
+
+
+        p5.fill(118,181,197)
+        p5.stroke(118,181,197)
+        p5.rect(0, 0, 5, height)
 
 
         p5.fill(118,181,197)
         p5.stroke(118,181,197)
-        p5.rect(0, 0, 20, height)
-
-
-        p5.fill(118,181,197)
-        p5.stroke(118,181,197)
-        p5.rect(width - 20, 0, 20, height)
+        p5.rect(width - 5, 0, 20, height)
 
         p5.fill(118,181,197)
         p5.stroke(118,181,197)
-        p5.rect(0, 0, width, 20)
+        p5.rect(0, 0, width, 5)
 
         p5.fill(226,135,67)
         p5.stroke(226,135,67)
-        p5.rect(0, height - 20, width, 20)
+        p5.rect(0, height - 1, width, 20)
 
 
     }
